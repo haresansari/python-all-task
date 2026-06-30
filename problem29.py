3ea5b36990d98f25ae4a5532468c8bd9bfcf6907
harisansari <haresansari007@gmail.com>
2026-06-30 12:26:49 +0530

    Delete problem29.py

--- problem29.py
+++
@@ -1,9 +0,0 @@
-balance = 5000
-
-amount = int(input("Enter amount: "))
-
-if amount <= balance:
-    balance -= amount
-    print("Remaining Balance =", balance)
-else:
-    print("Insufficient Balance")
