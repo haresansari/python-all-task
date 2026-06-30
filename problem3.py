beb4617c6a5f8cf653229c30eca7674c3d15b862
harisansari <haresansari007@gmail.com>
2026-06-30 12:27:53 +0530

    Delete problem3.py

--- problem3.py
+++
@@ -1,9 +0,0 @@
-# Calculator Program
-
-a = int(input("Enter first number: "))  # Input first number
-b = int(input("Enter second number: ")) # Input second number
-
-print("Addition =", a + b)      # Add
-print("Subtraction =", a - b)   # Subtract
-print("Multiplication =", a * b)# Multiply
-print("Division =", a / b)      # Divide 
